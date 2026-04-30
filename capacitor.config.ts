b305912ef46a8093a1365fd0e5fe60082a72dc21
Jonathan <j.ramrez12.j@gmail.com>
2026-04-29 20:40:46 -0600

    ZIP con Nombre de App actualizado

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'io.criptomensajes.steam',
-  appName: 'EncriptacionMensajes',
+  appName: 'CriptoMensajes',
   webDir: 'dist'
 };
 
